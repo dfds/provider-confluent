d148c84ac7fdd42077112e1917f176b07e497477
Emil H. Clausen <sequoiia@hummel.yt>
2021-11-29 14:18:23 +0100

    Fixed formatting

--- internal/clients/schemaregistry/schemaregistry_test.go
+++ internal/clients/schemaregistry/schemaregistry_test.go
@@ -3,107 +3,85 @@ package schemaregistry
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/google/go-cmp/cmp"
-
-	"github.com/dfds/provider-confluent/apis/schemaregistry/v1alpha1"
-	"github.com/dfds/provider-confluent/internal/clients/schemaregistry"
 	"github.com/dfds/provider-confluent/internal/clients/schemaregistry/commands"
 )
 
 func TestDescribeSchemaRegistryCommand(t *testing.T) {
-	var describeCommand = NewDescribeSchemaRegistryCommand("subject", "version", "environment", "key", "secret")
+	var describeCommand = commands.NewDescribeSchemaRegistryCommand("subject", "version", "environment", "key", "secret")
 
-	if(describeCommand.Args[4] != "subject")
-	{
+	if (describeCommand.Args[4] != "subject") {
 		t.Errorf("Subject is not in correct index")
 	}
 	
-	if(describeCommand.Args[6] != "version")
-	{
+	if(describeCommand.Args[6] != "version") {
 		t.Errorf("Version is not in correct index")
 	}
 	
-	if(describeCommand.Args[8] != "environment")
-	{
+	if(describeCommand.Args[8] != "environment") {
 		t.Errorf("Environment is not in correct index")
 	}
 	
-	if(describeCommand.Args[10] != "key")
-	{
+	if(describeCommand.Args[10] != "key") {
 		t.Errorf("Key is not in correct index")
 	}
 	
-	if(describeCommand.Args[12] != "secret")
-	{
+	if(describeCommand.Args[12] != "secret") {
 		t.Errorf("Secret is not in correct index")
 	}
 }
 
 func TestDeleteSchemaRegistryCommand(t *testing.T) {
-	var describeCommand = NewDeleteSchemaRegistryCommand("subject", "version", true, "environment", "key", "secret")
+	var describeCommand = commands.NewDeleteSchemaRegistryCommand("subject", "version", true, "environment", "key", "secret")
 
-	if(describeCommand.Args[4] != "subject")
-	{
+	if(describeCommand.Args[4] != "subject") {
 		t.Errorf("Subject is not in correct index")
 	}
 	
-	if(describeCommand.Args[6] != "version")
-	{
+	if(describeCommand.Args[6] != "version") {
 		t.Errorf("Version is not in correct index")
 	}
 	
-	if(describeCommand.Args[8] != "environment")
-	{
+	if(describeCommand.Args[8] != "environment") {
 		t.Errorf("Environment is not in correct index")
 	}
 	
-	if(describeCommand.Args[10] != "key")
-	{
+	if(describeCommand.Args[10] != "key") {
 		t.Errorf("Key is not in correct index")
 	}
 	
-	if(describeCommand.Args[12] != "secret")
-	{
+	if(describeCommand.Args[12] != "secret") {
 		t.Errorf("Secret is not in correct index")
 	}
 	
-	if(describeCommand.Args[13] != "--permanent")
-	{
+	if(describeCommand.Args[13] != "--permanent") {
 		t.Errorf("Permanent flag is not set at correct index")
 	}
 }
 
-func TestDeleteSchemaRegistryCommand(t *testing.T) {
-	var describeCommand = NewCreateSchemaRegistryCommand("subject", "schema", "schemaType", "environment", "key", "secret")
+func TestCreateSchemaRegistryCommand(t *testing.T) {
+	var describeCommand = commands.NewCreateSchemaRegistryCommand("subject", "schema", "schemaType", "environment", "key", "secret")
 
-	if(describeCommand.Args[4] != "subject")
-	{
+	if(describeCommand.Args[4] != "subject") {
 		t.Errorf("Subject is not in correct index")
 	}
 	
-	if(describeCommand.Args[6] != "schema")
-	{
+	if(describeCommand.Args[6] != "schema") {
 		t.Errorf("Schema is not in correct index")
 	}
 	
-	if(describeCommand.Args[8] != "schemaType")
-	{
+	if(describeCommand.Args[8] != "schemaType") {
 		t.Errorf("SchemaType is not in correct index")
 	}
 
-	if(describeCommand.Args[10] != "environment")
-	{
+	if(describeCommand.Args[10] != "environment") {
 		t.Errorf("Environment is not in correct index")
 	}
 	
-	if(describeCommand.Args[12] != "key")
-	{
+	if(describeCommand.Args[12] != "key") {
 		t.Errorf("Key is not in correct index")
 	}
 	
-	if(describeCommand.Args[14] != "secret")
-	{
+	if(describeCommand.Args[14] != "secret") {
 		t.Errorf("Secret is not in correct index")
 	}
 }
