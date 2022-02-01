019dcd8fe6370caeb5a2bdc7a391f4d16471365b
Willi Carlsen <carlsenwilli@gmail.com>
2022-02-01 15:01:38 +0100

    Fix apis serviceaccount lint

--- apis/serviceaccount/v1alpha1/serviceaccount_types.go
+++ apis/serviceaccount/v1alpha1/serviceaccount_types.go
@@ -33,7 +33,7 @@ type ServiceAccountStatus struct {
 
 // +kubebuilder:object:root=true
 
-// Schema is an example API type.
+// ServiceAccount is an example API type.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
