c39273743b192bc51d676763a468510ffabee220
Willi Carlsen <carlsenwilli@gmail.com>
2022-02-01 14:48:48 +0100

    Controller acl linter fix

--- internal/controller/acl/acl.go
+++ internal/controller/acl/acl.go
@@ -190,12 +190,12 @@ func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 				ResourceExists:    false,
 				ConnectionDetails: managed.ConnectionDetails{},
 			}, nil // returning nil because we want create on not found
-		} else {
-			return managed.ExternalObservation{
-				ResourceExists:    false,
-				ConnectionDetails: managed.ConnectionDetails{},
-			}, err
 		}
+		return managed.ExternalObservation{
+			ResourceExists:    false,
+			ConnectionDetails: managed.ConnectionDetails{},
+		}, err
+
 	}
 
 	// Diff
